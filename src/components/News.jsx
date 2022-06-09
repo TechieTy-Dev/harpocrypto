82ab28ef825445d7c1d3156aefe49e8a36f61337
Tyler Smith <Tysocialhq@gmail.com>
2022-06-08 22:36:54 -0500

    updating blog content

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -16,16 +16,18 @@ const News = () => {
         <Card hoverable className='news-card'>
           <a href="/">
             <div className='news-image-container'>
-              <Title className="news-title" level={4}>News Name</Title>
+              <Title className="news-title" level={4}>
+NEWS
+FINTECH STARTUP EDGE RELEASE FIRST CONFIDENTIAL BITCOIN MASTERCARD</Title>
 
             </div>
-            <p>News Description</p>
+            <p>Edges new card will not collect KYC data used to determine a persons identity giving complete privacy and instant transfers with no fees.</p>
             <div className="provider-container">
               <div>
                 <Avatar src={bm} alt="" />
-                <Text className="provider-name">News Provider</Text>
+                <Text className="provider-name">Bitcoin Magazine</Text>
               </div>
-              <Text>Data Published</Text>
+              <Text>06/08/2022</Text>
             </div>
           </a>
         </Card>
@@ -34,16 +36,16 @@ const News = () => {
         <Card hoverable className='news-card'>
           <a href="/">
             <div className='news-image-container'>
-              <Title className="news-title" level={4}>News Name</Title>
+              <Title className="news-title" level={4}>Trade thousands of tokens on your choice of network in Coinbase Wallet</Title>
 
             </div>
-            <p>News Description</p>
+            <p>Coinbase Wallet makes it simple to trade tokens on your choice of network. This month, we expanded our trading features to support token swaps on BNB Chain and Avalanche.</p>
             <div className="provider-container">
               <div>
                 <Avatar src={cb} alt="" />
-                <Text className="provider-name">News Provider</Text>
+                <Text className="provider-name">The Coinbase Blog</Text>
               </div>
-              <Text>Data Published</Text>
+              <Text>05/23/2022</Text>
             </div>
           </a>
         </Card>
@@ -52,15 +54,15 @@ const News = () => {
         <Card hoverable className='news-card'>
           <a href="/">
             <div className='news-image-container'>
-              <Title className="news-title" level={4}>News Name</Title>
+              <Title className="news-title" level={4}>BITCOIN IS THE ANSWER TO FINANCIAL BURNOUT</Title>
             </div>
-            <p>News Description</p>
+            <p>Burnout is experienced in numerous fields and finance is no exception. Bitcoin offers a viable and non-correlated alternative to a system known for burnout.</p>
             <div className="provider-container">
               <div>
                 <Avatar src={bm} alt="" />
-                <Text className="provider-name">News Provider</Text>
+                <Text className="provider-name">Bitcoin Magazine</Text>
               </div>
-              <Text>Data Published</Text>
+              <Text>06/07/2022</Text>
             </div>
           </a>
         </Card>
@@ -68,15 +70,15 @@ const News = () => {
         <Card hoverable className='news-card'>
           <a href="/">
             <div className='news-image-container'>
-              <Title className="news-title" level={4}>News Name</Title>
+              <Title className="news-title" level={4}>Coinbase Commits $1 Million for Public Goods in partnership with Gitcoin</Title>
             </div>
-            <p>News Description</p>
+            <p>Coinbase commits $1 million for Public Goods through Gitcoin, including $200,000 for upcoming Grants Round 14.</p>
             <div className="provider-container">
               <div>
                 <Avatar src={cb} alt="" />
-                <Text className="provider-name">News Provider</Text>
+                <Text className="provider-name">The Coinbase Blog</Text>
               </div>
-              <Text>Data Published</Text>
+              <Text>06/01/2022</Text>
             </div>
           </a>
         </Card>
@@ -85,15 +87,15 @@ const News = () => {
         <Card hoverable className='news-card'>
           <a href="/">
             <div className='news-image-container'>
-              <Title className="news-title" level={4}>News Name</Title>
+              <Title className="news-title" level={4}>365 DAYS OF FINANCIAL FREEDOM: THE STORIES OF BITCOIN ADOPTION IN EL SALVADOR</Title>
             </div>
-            <p>News Description</p>
+            <p>A year after the introduction of bitcoin as legal tender in El Salvador, we can reflect on the stories of those who’ve experienced the adoption firsthand.</p>
             <div className="provider-container">
               <div>
                 <Avatar src={bm} alt="" />
-                <Text className="provider-name">News Provider</Text>
+                <Text className="provider-name">Bitcoin Magazine</Text>
               </div>
-              <Text>Data Published</Text>
+              <Text>06/8/2022</Text>
             </div>
           </a>
         </Card>
@@ -102,15 +104,15 @@ const News = () => {
         <Card hoverable className='news-card'>
           <a href="/">
             <div className='news-image-container'>
-              <Title className="news-title" level={4}>News Name</Title>
+              <Title className="news-title" level={4}>Building a more open financial system: How Coinbase detects bad actors</Title>
             </div>
-            <p>News Description</p>
+            <p>At Coinbase, we take our responsibility to build a more open, accessible financial system very seriously. We’re deeply committed to our asset listing policies and processes, and we’ll continue to innovate as our dynamic space evolves.</p>
             <div className="provider-container">
               <div>
                 <Avatar src={cb} alt="" />
-                <Text className="provider-name">News Provider</Text>
+                <Text className="provider-name">The Coinbase Blog</Text>
               </div>
-              <Text>Data Published</Text>
+              <Text>05/19/2022</Text>
             </div>
           </a>
         </Card>
